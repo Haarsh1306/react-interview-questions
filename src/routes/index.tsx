cfe00bca108ecb61a1a36f97f2881a58cec46dd0
Harsh Verma <hv41096@gmail.com>
2024-07-31 23:18:00 +0530

    project structure for routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "../pages/Home"
+import { Search } from "../pages/Search"
+import { InfiniteScroll } from "../pages/InfiniteScroll"
+import { GridLight } from "../pages/GridLight"
+import { DragNotes } from "../pages/DragNotes"
+import { Stepper } from "../pages/Stepper"
+import { PasswordGenerator } from "../pages/PasswordGenerator"
+import { Error } from "../pages/Error"
+
+export const AppRoute = () =>{
+    return (
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+          <Route path="/infinite-scroll" element={<InfiniteScroll />}></Route>
+          <Route path="/grid-light" element={<GridLight />}></Route>
+          <Route path="/drag-notes" element={<DragNotes />}></Route>
+          <Route path="/stepper" element={<Stepper />}></Route>
+          <Route
+            path="/password-generator"
+            element={<PasswordGenerator />}
+          ></Route>
+          <Route
+            path="/password-generator"
+            element={<PasswordGenerator />}
+          ></Route>
+          <Route path="/*" element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
+    )
+}
